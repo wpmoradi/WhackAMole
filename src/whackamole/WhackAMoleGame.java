fb3ef4779ac1619d64078b0162db7ce8254ae9dc
Patriss Wais Moradi <wpmoradi@gmail.com>
2018-09-08 11:25:25 -0700

    implemented whack(x,y) method

--- src/whackamole/WhackAMoleGame.java
+++ src/whackamole/WhackAMoleGame.java
@@ -29,6 +29,9 @@ public class WhackAMoleGame {
     // TODO implement boolean place(int x, int y);
     // getter & setter functions
     public boolean place(int x, int y){
+        // adjust for 0 indexing
+        x--;
+        y--;
             // check if slot is empty
          if (this.molGrid[x][y] == 0){
              // code for empty slot
@@ -39,6 +42,7 @@ public class WhackAMoleGame {
              this.molesLeft++;
              System.out.println("Now there are a total of " +
                      this.molesLeft + " left.");
+             System.out.println("\n");
              return true;
 
                 }else{
@@ -47,6 +51,29 @@ public class WhackAMoleGame {
                     System.out.println("There is a mole here!");
                     return false;
                 }
+    }
+
+    // TODO need to implement a mole counter
+
+
+    public void whack(int x, int y){
+        // TODO implement whack method
+        x --; y--; //adjust for 0 indexing
+        if (this.molGrid[x][y] == 'M'){
+            //code for true case
+            System.out.println("AHA!!! Here is a mole!!");
+            this.score ++;
+            this.molesLeft--;
+            System.out.println("There are " + this.molesLeft + " moles left.");
+            System.out.println("Your score is now " + this.score + ".");
+        }else{
+            //code for false case
+        }
+
+        System.out.println("you had " + this.attemptsLeft + " attempts left.");
+        this.attemptsLeft--;
+        System.out.println("now you have " + this.attemptsLeft + " attempts left.");
+
     }
 
 
@@ -65,25 +92,10 @@ public class WhackAMoleGame {
         System.out.println("\n"); //create line space
 
 
+        wais.place(4,6);
+        wais.place(10,10);
+        wais.place(1,5);
 
-        // moleGridB
-        /***
-         * Can find array dimensions via .:
-         * rows by: moleGrid.length
-         * columns by: moleGrid[0].length
-         */
-
-        // using for loop - input 'M' in each slot of the 2D array
-        for (char index = 0; index < wais.molGrid.length; index++){
-            for (char iB = 0; iB<wais.molGrid[0].length; iB++){
-                wais.molGrid[index][iB] = 'M';
-            }
-        }
-
-
-
-        wais.molGrid[0][0] = 'e';
-        wais.place(2,3);
         // print the 2D array
         for (char[] a : wais.molGrid){
             for (char i : a){
@@ -92,8 +104,31 @@ public class WhackAMoleGame {
             System.out.println("\n");
         }
 
-        System.out.println("Below is MoleGridB: " +
-                Arrays.deepToString(wais.molGrid));
+        // attempt to whack moles
+        wais.whack(2,2);
+        wais.whack(10,10);
+
+        /***
+         * Can find array dimensions via .:
+         * rows by: moleGrid.length
+         * columns by: moleGrid[0].length
+         */
+
+        // using for loop - input 'M' in each slot of the 2D array
+//        for (char index = 0; index < wais.molGrid.length; index++){
+//            for (char iB = 0; iB<wais.molGrid[0].length; iB++){
+//                wais.molGrid[index][iB] = '*';
+//            }
+//        }
+
+
+        // print the 2D array
+        for (char[] a : wais.molGrid){
+            for (char i : a){
+                System.out.print(i + "\t");
+            }
+            System.out.println("\n");
+        }
 
     }
 
